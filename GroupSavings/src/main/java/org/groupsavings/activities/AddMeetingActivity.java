7883122ff7b9f5fb75ce76eb62ce77e2b0518c32
Shashank Singh <shashanksingh28@gmail.com>
2014-03-30 22:47:23 +0530

    testing merge

---
+++ GroupSavings/src/main/java/org/groupsavings/activities/AddMeetingActivity.java
@@ -0,0 +1,57 @@
+package org.groupsavings.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TableLayout;
+
+import org.groupsavings.R;
+import org.groupsavings.domain.Group;
+import org.groupsavings.domain.Member;
+
+import java.util.ArrayList;
+
+public class AddMeetingActivity extends Activity {
+
+    int groupId;
+    Group group;
+    ArrayList<Member> groupMembers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_meeting);
+        groupId = getIntent().getIntExtra(GroupLandingActivity.INTENT_EXTRA_GROUP, 0);
+
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.table_new_meeting);
+
+
+    }
+
+    private void populateMeetingTransactions(TableLayout tableLayout) {
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.add_meeting, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
