9fb926e82c18635510cf2b1f04de38f0134fbfa5
rohan <rohansadale@gmail.com>
2014-06-15 22:28:26 +0530

    Added all fields in Members

---
+++ GroupSavings/src/main/java/org/groupsavings/domain/MeetingDetails.java
@@ -0,0 +1,12 @@
+package org.groupsavings.domain;
+
+/**
+ * Created by rohan on 15/6/14.
+ */
+public class MeetingDetails {
+
+    public String MeetingId;
+    public String MemberId;
+    public String IsAbsent;
+    public float Fine;
+}
