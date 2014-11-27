bd46b7fc94f594e001cda00af9a5f97632c3adc2
Rohan Sadale <rohansadale@gmail.com>
2014-11-27 13:33:39 +0530

    Create DbConfig.php

---
+++ server/DbConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Put all database connection variables
+ */
+
+define('DB_USER', "a5150163_planind"); // db user
+define('DB_PASSWORD', "planindia123"); // db password (mention your db password here)
+define('DB_DATABASE', "a5150163_planind"); // database name
+define('DB_SERVER', "mysql12.000webhost.com"); // db server
+
+?>
